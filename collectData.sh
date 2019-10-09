89492341289a843f2d3d39792f28948cf83472e7
zicol <zicol@192.168.0.29>
2019-10-09 23:09:20 +0300

    changed testset added collection script

--- collectData.sh
+++ collectData.sh
@@ -2,11 +2,18 @@
 
 javac -d bin src/**/*.java
 
-for i in {1..2}
+rm -rf results
+mkdir results
+
+for iii in {1..12}
 do
-	echo "Heuristic number: $i "
-	for ii in {0..2}
+	for ii in {1..2}
 	do
-		java -cp ./bin Sokoban/SokobanAstarSearch $i input.txt
+		echo "Heuristic number: $ii "
+		for i in {1..10}
+		do
+			echo "Problem number: $i "
+			java -cp ./bin Sokoban/SokobanAstarSearch $ii problem$i.txt >> results/result_$i_$ii.txt
+		done
 	done
 done
